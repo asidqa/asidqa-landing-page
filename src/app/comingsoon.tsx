6bfdf1095ae5e305a385fa09f7b569f23d4a73d4
Muhammad Tayyab Sheikh <cstayyab@gmail.com>
2023-12-08 18:25:54 +0500

    🎉 Landing Page for Asidqa.com

---
+++ src/app/comingsoon.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import { Button, Input, Textarea } from '@/components/ui/';
+import AsidqaLogo from '@/components/logo';
+
+export default function LaunchingSoon() {
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [message, setMessage] = useState('');
+
+
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-black-background text-white-foreground">
+            <AsidqaLogo width={256} height={256} />
+            <h1 className="text-5xl font-bold mb-2">Tick-Tock, Tech Unlocked Soon!</h1>
+            <p className="text-xl mb-6">Be the first to experience our cutting-edge IT solutions.</p>
+            <form action={"mailto:contact@asidqa.com"} className="flex flex-col gap-4 w-1/3">
+                <div className='flex flex-row items-center gap-2'>
+                    <Input required type="text" placeholder="Enter your name" name={"Name"} value={name} onChange={(e) => setName(e.target.value)} />
+                    <Input
+                        required
+                        type="email"
+                        placeholder="Enter your email address"
+                        value={email}
+                        name={"Email"}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <Textarea
+                    required
+                    placeholder="Your inquiries or comments"
+                    value={message}
+                    name={"Message"}
+                    onChange={(e) => setMessage(e.target.value)}
+                />
+                <span className="text-sm text-gray-400">We respect your privacy. Your email address will never be shared or sold.</span>
+                <Button type="submit" variant="default">Stay Updated</Button>
+                <span className="text-sm text-gray-400 text-center">You can also email us directly at <a href='mailto:contact@asidqa.com'>contact@asidqa.com</a></span>
+            </form>
+        </div>
+    );
+}
---
+++ src/components/ui/index.ts
@@ -0,0 +1,29 @@
+export { Button, buttonVariants } from './button';
+export { Input } from './input';
+export { Label } from './label';
+export { Textarea } from './textarea';
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './card';
+export {
+    navigationMenuTriggerStyle,
+    NavigationMenu,
+    NavigationMenuList,
+    NavigationMenuItem,
+    NavigationMenuContent,
+    NavigationMenuTrigger,
+    NavigationMenuLink,
+    NavigationMenuIndicator,
+    NavigationMenuViewport,
+} from './navigation-menu';
+export {
+    useFormField,
+    Form,
+    FormItem,
+    FormLabel,
+    FormControl,
+    FormDescription,
+    FormMessage,
+    FormField,
+} from './form';
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from './accordion';
+export { Tabs, TabsList, TabsTrigger, TabsContent } from './tabs';
+
